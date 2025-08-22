ba06e9cac2e8fd4671fbe5d0b6fe1398eac98dd9
helo115 <harishuusain@gmail.com>
2025-08-22 13:39:59 +0500

    Remove Assignment3 from index

--- Assignment/Assignment3/part1 Q7.py
+++
@@ -1,7 +0,0 @@
-#7.Write a function to take number as argument and return the square of that number.
-number=int(input("Enter the number : "))
-def square(number):
-    result=number*number
-    return result
-hi=square(number)
-print(hi)
