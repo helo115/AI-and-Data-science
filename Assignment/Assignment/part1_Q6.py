48624d47547bd373c4d9df61ff8e5f7b6acf5d74
helo115 <harishuusain@gmail.com>
2025-08-22 12:43:12 +0500

    Initial commit: Added Assignment folder with subfolders

--- Assignment/Assignment/part1_Q6.py
+++
@@ -1,8 +0,0 @@
-#6. Write a program to take integer as input and check if it is even or odd.
-numm =int(input("Enter number :"))
-if numm % 2==0:
-    print("The number is even")
-elif numm%3==0:
-    print("The number is odd")
-else:
-    print("not even or odd")
--- Assignment/Assignment/part2_Q6.py
+++
@@ -1,5 +0,0 @@
-
-#6.	Write a program to print each character of a string.
-
-for i in "String":
-    print(i)
